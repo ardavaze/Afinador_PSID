7e6632decb95d142794a6982c6b7fcf0b3ef4d1c
karol <karol@192.168.0.8>
2023-09-28 19:19:31 -0500

    a

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ close all;
 [x,Fs]=audioread('audio_mi.wav');
 sound(x,Fs)
 x1=x(:,1);
-%señal en el dominio del tiempo
+%señal captada en el dominio del tiempo
 figure 
 plot(x1)
 N=20000;
@@ -14,18 +14,36 @@ N=length(x1);
 n=0:N-1;
 k=n;
 fk=k*Fs/N;
+%señal en el dominio de la frecuencia
 figure
 plot(fk(1:N/2),abs(X1(1:N/2)))
-[mm, ii]=max(abs(X1(1:N/2)));
-(ii-1)*Fs/N
+% [mm, ii]=max(abs(X1(1:N/2)));
+% (ii-1)*Fs/N;
+%potencia espectral 
+pot=abs(X1).^2;
+[a,b]=max(pot);
+fo=fk(b);
+figure
+plot(fk,pot)
+xlabel('frecuencia')
+ylabel('pot espectral')
+c=linspace(0,5);
+
 %instrumento =0 ->guitarra
 %instrumento =1 ->violin
-Instrumento = 0
-Nota=1
+
+Instrumento = 0;
+Nota=1;
 if Instrumento == 0
     switch Nota
         case 1 % E
             freq_afin = 329.6 ; 
+            figure
+            d=329.6*ones(size(c));
+            plot(d,c,'r','lineWidth',2)
+            e=fo*ones(size(c));
+            hold on 
+            plot(e,c,'b','lineWidth',2)
         case 2 % B
             freq_afin = 246.9;
         case 3 % G
